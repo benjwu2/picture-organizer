78069484335f172df1f12097e41405388c3dd4cd
benjwu2 <benjwu2@gmail.com>
2023-07-18 19:58:27 -0400

    add ability to exit returnFullAddress recursion by inputting cancel

--- testUnzip.py
+++ testUnzip.py
@@ -1,13 +1,34 @@
 from zipfile import ZipFile
-import os
 from addressArray import array
+import os
+import sys
+
+
+def returnFullAddress(streetNumber):
+    if streetNumber == "cancel":
+        sys.exit("Canceled by user")
+    matches = []
+    for address in array:
+        if address.split(" ")[0] == streetNumber:
+            matches.append(address)
+    if len(matches) == 0:
+        print("There are no addresses with this street number")
+        retry = input("Enter the street number (enter cancel to exit): ")
+
+        return returnFullAddress(retry)
+    elif len(matches) < 2:
+        print(matches[0])
+        return matches[0]
+    else:
+        print("There are multiple addresses with this street number: {}".format(matches))
+
 
 # right click the zip file to be unzipped and select "Copy as path"
 # this is what is to be pasted into command prompt when prompted
 
 # the slice [1:-1] gets rid of the quotes around the pasted file path, as the input method
 # will add another pair of quotes around what is inputted, making ZipFile unable to read the path
-# address = input("property address?")
+address = returnFullAddress(input("Enter the street number: "))
 # filePath = input("Enter the file path (with quotes): ")[1:-1]
 
 # os.mkdir("./{}".format(address))
@@ -18,15 +39,4 @@ from addressArray import array
 
 
 
-def returnFullAddress(streetNumber):
-    matches = []
-    for address in array:
-        if address.split(" ")[0] == streetNumber:
-            matches.append(address)
-    if len(matches) == 0:
-        print("There are no addresses with this street number")
-    elif len(matches) < 2:
-        print(matches[0])
-        return matches[0]
-    else:
-        print("There are multiple addresses with this street number: {}".format(matches))
+print(address)
