e8a86bf25e8032e516438c24a31432e00ddd926d
benjwu2 <benjwu2@gmail.com>
2023-07-18 22:03:50 -0400

    refactor: comments added

--- testUnzip.py
+++ testUnzip.py
@@ -3,26 +3,38 @@ from addressArray import array
 import os
 import sys
 
-
+# returns a full address from the array of addresses in addressArray.py (array) that has a
+# street number that matches the passed number. If there are multiple addresses in array
+# that start with the same street number, the user is prompted to select the address that
+# they want to use
 def returnFullAddress(streetNumber):
     if streetNumber == "cancel":
         sys.exit("\nCanceled by user")
+
+    # array to accumalate matching addresses
     matches = []
 
+    # iterates through the array looking for addresses that have a street number that matches the input
     for address in array:
+        # address.split(" "[0]) isolates the street number from the current address
         if address.split(" ")[0] == streetNumber:
             matches.append(address)
     if len(matches) == 0:
         print("\nThere are no addresses with this street number")
         retry = input("Enter the street number (enter \"cancel\" to exit): ")
 
+        # recursion
         return returnFullAddress(retry)
+    
+    # if there is only one match, return it
     elif len(matches) < 2:
         return matches[0]
     else:
         print("\nThere are multiple addresses with this street number: {}".format(matches))
         selector = None
 
+        # selector is the *valid* option picked by the user. userInput is the possibly invalid option picked.
+        # if userInput passes the check, it is set as the value of selector
         while selector == None:
             listLength = len(str(matches))
             userInput = int(input("Select one of the matches by entering the corresponding number \"1\" for the first option, \"2\" for the second, etc: "))
