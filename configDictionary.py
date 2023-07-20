c76b0b133799543120776339c0a7ef742fce9103
benjwu2 <benjwu2@gmail.com>
2023-07-19 20:50:12 -0400

    refactor: add comments to configDictionary. NOTE: commit aa54dd6 actually was for an edit to what the second key in the config dictionary, not the changes described

--- configDictionary.py
+++ configDictionary.py
@@ -1,1 +1,5 @@
+# this dictionary is used to control the behavior of testUnzip.py functions.
+# For example, arrayToUse controls what array of addresses in addressArray.py is itereated through by 
+# returnFullAddress in testUnzip.py
+# The value of arrayToUse should be the name of the array to use as a string
 config = {"defaultDest": "./tempFolderName", "arrayToUse": "array"}
