e6f5bc2873f4ba452b3135b38becd861cb8cd23a
benjwu2 <benjwu2@gmail.com>
2023-07-19 21:55:34 -0400

    add function getArrayNum

--- addressCSVParse.py
+++ addressCSVParse.py
@@ -1,5 +1,18 @@
 fileAddress = input("enter the file path for the CSV file to be parsed (with quotes): ")[1:-1]
 
+def getArrayNum():
+    with open("addressArray.py") as file:
+        contents = file.read()
+
+        # if the file is blank, then the array should just be called "array" with no added number
+        if contents == "": return ""
+        
+        # the number of times "array" appears in the file is equal to the number
+        # of elements in the array made of the string split on "array," minus 1
+        return len(contents.split("array")) - 1
+
+
+
 
 
 
